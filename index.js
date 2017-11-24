c3164b1e51d5f58c04d68320027640ae534ae41d
Adam Fisher <ae.fisher.gains@gmail.com>
2017-11-24 14:43:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function breakOut (array, changeValue, stopValue) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue
+    } else {
+      break 
+    }
+  }
+  return array
+}
